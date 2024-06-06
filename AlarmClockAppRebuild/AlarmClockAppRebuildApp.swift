251bc9447252b88cce2c25ef881f1d147abc40c5
Matthew Zhang <gzhang@lcs.on.ca>
2024-06-06 12:46:53 -0400

    Initial Commit

---
+++ AlarmClockAppRebuild/AlarmClockAppRebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockAppRebuildApp.swift
+//  AlarmClockAppRebuild
+//
+//  Created by GengYu Zhang on 2024-06-06.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockAppRebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
