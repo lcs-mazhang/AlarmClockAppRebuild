a261325920f9c1092c3a81717eb0750092fd6f46
Matthew Zhang <gzhang@lcs.on.ca>
2024-06-07 15:20:47 -0400

    Finished basic settingView layout

---
+++ AlarmClockAppRebuild/Views/AlarmSoundsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmSoundsView.swift
+//  AlarmClockAppRebuild
+//
+//  Created by GengYu Zhang on 2024-06-07.
+//
+
+import SwiftUI
+
+struct AlarmSoundsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmSoundsView()
+}
