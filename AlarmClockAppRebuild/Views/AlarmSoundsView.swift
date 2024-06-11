f929759a51d48236e31ac58892def7cf046518a9
Matthew Zhang <gzhang@lcs.on.ca>
2024-06-11 08:47:13 -0400

    Created the Navigation link for settingView

--- AlarmClockAppRebuild/Views/AlarmSoundsView.swift
+++ AlarmClockAppRebuild/Views/AlarmSoundsView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct AlarmSoundsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                List {
+                    
+                    
+                }
+                .listStyle(.plain)
+                Spacer()
+            }
+            .navigationTitle("Sounds & Haptics")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
