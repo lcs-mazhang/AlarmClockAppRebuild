0b77208355799f4a7ff8de7f017d47a7cfdc527d
Matthew Zhang <gzhang@lcs.on.ca>
2024-06-13 12:22:49 -0400

    Creating array for the timepicker(Data picker) that I will add

--- AlarmClockAppRebuild/Views/AlarmView.swift
+++ AlarmClockAppRebuild/Views/AlarmView.swift
@@ -8,40 +8,44 @@
 import SwiftUI
 
 struct AlarmView: View {
+    
+    // Stored properties
+    @State var alarmList = [Date]()
+    @State var timepicker = false
+    @State var selectedTime = Date()
+    
     var body: some View {
         NavigationStack {
             VStack {
                 List {
-//                    Text("6:30 AM")
-//                    Text("9:30 AM")
-                }
-                .listStyle(.plain)
-                Spacer()
-            }
-            .navigationTitle("Alarm")
-            .navigationBarTitleDisplayMode(.inline)
-            .bold()
-            .toolbar {
-                // Top Tool bar item
-                ToolbarItem(placement: .navigationBarLeading) {
-                    // Adding alarm
-                    Button {
-                        // Func
-                    } label: {
-                        Image(systemName: "plus")
                     }
+                    .listStyle(.plain)
+                    Spacer()
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    // Edit alarm
-                    Button ("Edit") {
-                        // Func
+                .navigationTitle("Alarm")
+                .navigationBarTitleDisplayMode(.inline)
+                .bold()
+                .toolbar {
+                    // Top Tool bar item
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        // Adding alarm
+                        Button {
+                            // Func
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        // Edit alarm
+                        Button ("Edit") {
+                            // Func
+                        }
                     }
                 }
             }
         }
     }
-}
-
-#Preview {
-    AlarmView()
-}
+    
+    #Preview {
+        AlarmView()
+    }
